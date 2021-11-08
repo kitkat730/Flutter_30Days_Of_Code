3e1c148c7e824d36a8788131493068dac10fd6f5
Hareram Shah <hareramsah786@gmail.com>
2021-11-08 15:54:34 +0530

    added scaffold widget

---
+++ lib/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Heading"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to Day 2"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
