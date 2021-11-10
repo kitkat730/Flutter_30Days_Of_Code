1350c664bef27effc6e31655a3e091b94b48158d
Hareram Shah <hareramsah786@gmail.com>
2021-11-10 11:38:35 +0530

    Learned about Stateful Widget, Animation

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/utils/Routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool bChangeState = false;
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -14,7 +21,7 @@ class LoginPage extends StatelessWidget {
                 height: 15,
                 //child: Text("ssg"),
               ),
-              Text("Welcome",
+              Text("Welcome $name",
                   style: TextStyle(
                     fontWeight: FontWeight.bold,
                     fontSize: 22,
@@ -23,13 +30,17 @@ class LoginPage extends StatelessWidget {
                 height: 12,
               ),
               Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 20.0, horizontal: 28.0),
+                padding: const EdgeInsets.symmetric(
+                    vertical: 20.0, horizontal: 28.0),
                 child: Column(
                   children: [
                     TextFormField(
                       decoration: InputDecoration(
                           hintText: "Enter User Name", labelText: "UserName"),
+                      onChanged: (String str) {
+                        name = str;
+                        setState(() {});
+                      },
                     ),
                     TextFormField(
                       decoration: InputDecoration(
@@ -39,10 +50,38 @@ class LoginPage extends StatelessWidget {
                     SizedBox(
                       height: 40,
                     ),
-                    ElevatedButton(
-                        style: TextButton.styleFrom(minimumSize: Size(120, 50)),
-                        onPressed: () { Navigator.pushNamed(context, MyRoutes.HomeRoute);},
-                        child: Text("Sign In")),
+                    InkWell(
+                      onTap: () async {
+                        setState(() {
+                          bChangeState = true;
+                        });
+                        await Future.delayed(Duration(seconds: 1));
+                        Navigator.pushNamed(context, MyRoutes.HomeRoute);
+                      },
+                      child: AnimatedContainer(
+                          duration: Duration(seconds: 1),
+                          height: 45,
+                          width: bChangeState ? 50 : 150,
+                          child: bChangeState
+                              ? Icon(Icons.done, color: Colors.white)
+                              : Text(
+                                  "Login",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 24),
+                                ),
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                            borderRadius:
+                                BorderRadius.circular(bChangeState ? 40 : 15),
+                            color: Colors.blue,
+                          )),
+                    )
+                    // ElevatedButton(
+                    //     style: TextButton.styleFrom(minimumSize: Size(120, 50)),
+                    //     onPressed: () { Navigator.pushNamed(context, MyRoutes.HomeRoute);},
+                    //     child: Text("Sign In")
+                    //     ),
                   ],
                 ),
               ),
