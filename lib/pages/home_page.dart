4fd55ab8f9a8b1e5c31554baf23877b70b0ac4da
Hareram Shah <hareramsah786@gmail.com>
2021-11-18 16:13:41 +0530

    all about grid tile and grid view

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'dart:convert';
 import 'package:flutter_application_1/models/catalog.dart';
-import 'package:flutter_application_1/widgets/Item_widget.dart';
+//import 'package:flutter_application_1/widgets/Item_widget.dart';
 import 'package:flutter_application_1/widgets/drawer.dart';
 
 class HomePage extends StatefulWidget {
@@ -46,14 +46,45 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child:( CatalogModel.items.length > 1 && CatalogModel.items != null ) ? ListView.builder(
-               itemCount:CatalogModel.items.length,
-               itemBuilder: (context, index){
-                 return ItemWidget(item: CatalogModel.items[index]);
-               },
-            ) : const Center(
-                child: CircularProgressIndicator(),
-            ),
+        child: GridView.builder(
+          itemCount: CatalogModel.items.length,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 12,
+
+          ), 
+          itemBuilder: (context, index) {
+            final Item = CatalogModel.items[index];
+            return Card(
+              clipBehavior: Clip.antiAlias,
+              shape:  RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+              child: GridTile(
+                child: Image.network(Item.image),
+                header: Container(
+                  child: Text(Item.name,style : TextStyle(
+                    color : Colors.white,
+                  )),
+                  padding: const EdgeInsets.all(12),
+                  decoration: BoxDecoration(
+                  color : Colors.deepPurple,
+                  )
+                  ,
+                  ),
+                footer: Text("\$${Item.price}",),
+                )
+            );
+             
+          }
+          ),
+
+        // child:( CatalogModel.items.length > 1 && CatalogModel.items != null ) ? ListView.builder(
+        //        itemCount:CatalogModel.items.length,
+        //        itemBuilder: (context, index){
+        //          return ItemWidget(item: CatalogModel.items[index]);
+        //        },
+        //     ) : const Center(
+        //         child: CircularProgressIndicator(),
+        //     ),
       ),
       drawer: MyDrawer(),
     );
